38845e4db0c05ed5374a8fe1824d52c47fd93ba9
Lia Tarar <lia.tarar@simplybusiness.co.uk>
2021-06-25 14:55:59 +0100

    route types

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  # (http://localhost:3000/demo/index) renders the hello world page
   get 'demo/index'
+
+  # root route, takes you to the root of the page
+  # (http://localhost:3000/) renders the hello world page
+  # root 'demo#index'
+
+  # default route, picks up index action in controller by default
+  #  (http://localhost:3000/demo/) renders the hello world page
+  # get ':controller(/:action(/:id))'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
